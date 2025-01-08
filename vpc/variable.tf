91e11e141fe07907de32b881c1fd4ef4038d2d71
awsdevopsbykumar <64312757+awsdevopsbykumar@users.noreply.github.com>
2025-01-08 11:51:31 +0530

    Update variable.tf

--- vpc/variable.tf
+++ vpc/variable.tf
@@ -6,5 +6,4 @@ variable "vpc_cidr_block" {}
 variable "vpc_instance_tenancy" {}
 variable "vpc_enable_dns_support" {}
 variable "vpc_enable_dns_hostnames" {}
-variable "vpc_enable_classiclink" {}
 variable "vpc_tags" {}
